a7dd238edc0ec76925b39090367133b9e8aa14af
Oskar Carlbaum <oskarcarlbaum@gmail.com>
2018-10-07 16:13:10 +0200

    fixup! Loading functions exported from a Vulkan Loader library

--- src/renderer/vulkan/vulkansetup.cpp
+++ src/renderer/vulkan/vulkansetup.cpp
@@ -62,12 +62,12 @@ bool TempVulkanSetupObject::initProcAddr()
 #define EXPORTED_VULKAN_FUNCTION( name )                              \
     name = (PFN_##name)LoadFunction( vulkan_library, #name );         \
     if( name == nullptr ) {                                           \
-      std::cout << "Could not load exported Vulkan function named: "  \
-        #name << std::endl;                                           \
-      return false;                                                   \
+        std::cout << "Could not load exported Vulkan function named: "\
+            #name << std::endl;                                       \
+        return false;                                                 \
     }
 
-#include "ListOfVulkanFunctions.inl"
+#include "listofvulkanfunctions.inl"
 
     return true;
 }
