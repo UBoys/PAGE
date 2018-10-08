79367e1e70fe7b925a62332c691357a829129a20
Michael Novén <michael.f.noven@gmail.com>
2018-10-08 23:53:19 +0200

    Create render instance in engine

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -2,14 +2,16 @@
 #include <iostream>
 
 #include "../display/glfw/displayglfw.h"
-
-#include "../renderer/irenderer.h"
+#include "pgfx.h"
 
 namespace page {
 
 Engine::Engine(const InitData data) {
 	std::cout << "PAGE engine initializing ...\n";
 	mDisplay = std::make_unique<DisplayGlfw>(data.width, data.height, data.title, data.fullScreen);
+
+	pgfx::MainRenderer r;
+	r.Foo();
 }
 
 Engine::~Engine() = default;
