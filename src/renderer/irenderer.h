ba67e57f4ec74a4627ed74b362588b56b4888a82
Michael Novén <michael.f.noven@gmail.com>
2018-10-08 09:27:27 +0200

    PR feedback

--- src/renderer/irenderer.h
+++ src/renderer/irenderer.h
@@ -1,11 +1,14 @@
 #pragma once
 
-namespace molten {
+#include <vector>
+
+namespace page::gfx {
 
 enum RendererType {
     Null,
     OpenGL,
     Vulkan,
+	D3D11,
     D3D12
 };
 
@@ -22,7 +25,6 @@ struct RendererCaps {
     RendererType type;
     void* allocator;
     bool debug;
-    bool msaa;
 };
 
 struct TextureDescription {};
@@ -32,10 +34,10 @@ struct PipelineState {};
 // TODO: Constant buffers
 
 // Handle for each renderer resource.
-// All rendering resources are owned the renderer and should not be coupled with client code
-struct MoltenObject {
-    MoltenObject() { objectId = ++s_objectIds;};
-    bool IsValid() { return objectId != 0; }
+// All rendering resources are owned by the renderer and should not be coupled with client code
+struct GfxObject {
+    GfxObject() { objectId = ++s_objectIds;};
+    bool IsValid() const { return objectId != 0; }
     uint32_t objectId = 0;
     static uint32_t s_objectIds;
 };
@@ -44,27 +46,27 @@ class IRenderer {
 public:
     virtual ~IRenderer() = default;
 
-    virtual void Initialize(const RendererCaps& caps);
+    virtual void Initialize(const RendererCaps& caps) = 0;
 
-    virtual MoltenObject CreateVertexBuffer(const uint32_t count) = 0;
-    virtual MoltenObject CreateIndexBuffer(uint32_t count) = 0;
-    virtual MoltenObject CreatePipelineState(const PipelineState& state);
-    virtual MoltenObject CreateTexture(const TextureDescription& description, const std::vector<void*> data);
-    virtual MoltenObject CreateSamplerState(const SamplerDescription& description);
+    virtual GfxObject CreateVertexBuffer(const uint32_t count) = 0;
+    virtual GfxObject CreateIndexBuffer(const uint32_t count) = 0;
+    virtual GfxObject CreatePipelineState(const PipelineState& state) = 0;
+    virtual GfxObject CreateTexture(const TextureDescription& description, const std::vector<void*>& data) = 0;
+    virtual GfxObject CreateSamplerState(const SamplerDescription& description) = 0;
 
-    virtual void* MapVertexBuffer(const MoltenObject& handle) = 0;
-    virtual void* MapIndexBuffer(const MoltenObject& handle) = 0;
-    virtual void UnmapVertexBuffer(const MoltenObject& handle, const uint32_t count) = 0;
-    virtual void UnmapIndexBuffer(const MoltenObject& handle, const uint32_t count) = 0; 
+    virtual void* MapVertexBuffer(const GfxObject& handle) = 0;
+    virtual void* MapIndexBuffer(const GfxObject& handle) = 0;
+    virtual void UnmapVertexBuffer(const GfxObject& handle, const uint32_t count) = 0;
+    virtual void UnmapIndexBuffer(const GfxObject& handle, const uint32_t count) = 0; 
 
-    virtual void DestroyVertexBuffer(const MoltenObject& handle) = 0;
-    virtual void DestroyIndexBuffer(const MoltenObject& handle) = 0;
-    virtual void DestroyTexture(const MoltenObject& handle) = 0;
-    virtual void DestroyPipelineState(const MoltenObject& handle) = 0;
-    virtual void DestroySamplerState(const MoltenObject& hanele) = 0;
+    virtual void DestroyVertexBuffer(const GfxObject& handle) = 0;
+    virtual void DestroyIndexBuffer(const GfxObject& handle) = 0;
+    virtual void DestroyTexture(const GfxObject& handle) = 0;
+    virtual void DestroyPipelineState(const GfxObject& handle) = 0;
+    virtual void DestroySamplerState(const GfxObject& hanele) = 0;
 
-    /// Actual rendering commands that operate on updated and ready resources.
-    virtual void Render(const std::vector<RenderCommand> commandBuffer) = 0;
+    // Actual rendering commands that operate on updated and ready resources.
+    virtual void Render(const std::vector<RenderCommand>& commandBuffer) = 0;
 };
 
-} 
+}
